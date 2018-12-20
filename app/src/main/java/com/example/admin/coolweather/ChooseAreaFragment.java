4a2b4e7ecf8c375eaf05fc8c650d107603a72e28
Joe <lzqjsk@hotmail.com'git config --global user.name git config --global user.email>
2018-12-20 15:39:56 +0800

    加入显示天气信息的功能.

--- app/src/main/java/com/example/admin/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/admin/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.admin.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -177,6 +178,23 @@ public class ChooseAreaFragment extends Fragment {
                     selectedCity=cityList.get(position);
                     queryCounties();
                 }
+
+                else if (currentLevel == LEVEL_COUNTY) {
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
+
+//                     if (getActivity() instanceof WeatherActivity) {
+//                        WeatherActivity activity = (WeatherActivity) getActivity();
+//                        activity.drawerLayout.closeDrawers();
+//                        activity.swipeRefresh.setRefreshing(true);
+//                        activity.requestWeather(weatherId);
+//                    }
+                }
+
+
             }
         });
 
--- app/src/main/java/com/example/admin/coolweather/android/util/Utility.java
+++ app/src/main/java/com/example/admin/coolweather/android/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.admin.coolweather.android.db.City;
 import com.example.admin.coolweather.android.db.County;
 import com.example.admin.coolweather.android.db.Province;
+import com.example.admin.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -46,7 +48,7 @@ public class Utility {
                     City city=new City();
                     city.setCityName(cityObject.getString("name"));
                     city.setCityCode(cityObject.getInt("id"));
-                    city.setProvinceId(cityObject.getInt("provinceId"));
+                    city.setProvinceId(provinceId);
                     city.save();
                 }
                 return true;
@@ -71,7 +73,7 @@ public class Utility {
                     County county=new County();
                     county.setCountyName(countyObject.getString("name"));
                     county.setWeatherId(countyObject.getString("weather_id"));
-                    county.setCityId(countyObject.getInt("cityId"));
+                    county.setCityId(cityId);
                     county.save();
                 }
                 return true;
@@ -83,7 +85,20 @@ public class Utility {
         return false;
     }
 
-
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
 }
